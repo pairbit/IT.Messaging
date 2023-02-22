46ff1604abb8ec900e4c5253cc601f93cb448553
Ivan Tikhonov <itikhonov@mail.com>
2023-02-22 18:26:15 +0300

    example

--- IT.Messaging.Tests/PublisherTest.cs
+++ IT.Messaging.Tests/PublisherTest.cs
@@ -10,54 +10,104 @@ public class PublisherTest
         _channel = channel;
     }
 
+    /// <summary>
+    /// Для простых систем подойдет такой режим, 
+    /// подписываемся на обработку одной очереди по умолчанию.
+    /// Недостаток состоит в том, если одна система создает 50% и более нагрузки, другие системы могут страдать.
+    /// </summary>
     [Test]
-    public void TestGuid()
+    public void Subscribe_Default()
     {
-        var guid = Guid.NewGuid();
+        _channel.Subscribe<Guid>(processDefault);
 
-        _channel.Subscribe((Guid guid, string? channel) =>
-        {
-            Console.WriteLine($"[{channel}] -> {guid}");
-        }, "POIB-*");
+        Publish(_channel);
 
-        _channel.Publish(guid, "POIB-hash");
+        Assert.Pass();
+    }
+
+    /// <summary>
+    /// Для разграничения, можно создать очередь под самую нагруженную систему отдельно
+    /// или под каждую систему
+    /// </summary>
+    [Test]
+    public void Subscribe_BySystem()
+    {
+        //_channel.Bind("eb.*", "eb");
+
+        _channel.Subscribe<Guid>(processDefault);
+        _channel.Subscribe<Guid>(processDefault, "eb");
+
+        Publish(_channel);
+
+        Assert.Pass();
+    }
+
+    /// <summary>
+    /// Очередь по операциям
+    /// </summary>
+    [Test]
+    public void Subscribe_ByOperation()
+    {
+        //_channel.Bind("*.enhance.*", "enhance");
+        //_channel.Bind("*.prepare.*", "prepare");
+        //_channel.Bind("*.generate-view.*", "generate-view");
+
+        _channel.Subscribe<Guid>(processDefault);
+        _channel.Subscribe<Guid>(enhanceSign, "enhance");
+        _channel.Subscribe<Guid>(prepareSign, "prepare");
+        _channel.Subscribe<Guid>(generateView, "generate-view");
+
+        Publish(_channel);
 
         Assert.Pass();
     }
 
     [Test]
-    public void TestGuidMulti()
+    public void Subscribe_Optimize()
     {
-        _channel.Subscribe((Guid[] guid, string? channel) =>
-        {
-            foreach (var g in guid)
-            {
-                Console.WriteLine($"[{channel}] -> {g}");
-            }
+        //при публикации "eb.generate-view-html.big" первое правило подходит и указан параметр остановить дальнейшую обработку
+        //идея данной очереди в том, что очень долгие запросы обрабатываются в отдельных потоках и не засоряют общие очереди
+        //_channel.Bind("*.big", "big", stop: true);
 
-        }, "POIB-*");
+        //_channel.Bind("eb.*", "eb");
 
-        _channel.Publish(new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }, "POIB-hash");
+        _channel.Subscribe<Guid>(processDefault);
+        _channel.Subscribe<Guid>(processDefault, "eb");
+        _channel.Subscribe<Guid>(processDefault, "big");
+
+        Publish(_channel);
 
         Assert.Pass();
     }
 
-    [Test]
-    public void SigningTest()
+    private void Publish(IPublisher publisher)
     {
-        _channel.Subscribe<Guid>(cleanRequest, "*-cleanRequest");
-        _channel.Subscribe<Guid>(verifyTransformSignature, "*-verifyTransformSignature");
-        _channel.Subscribe<Guid>(enhanceSign, "*-enhanceSign");
+        publisher.Publish(new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }, "eb.enhance.normal");
+        publisher.Publish(new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }, "eb.enhance.small");
+        publisher.Publish(new[] { Guid.NewGuid() }, "eb.generate-view-html.big");
+        publisher.Publish(new[] { Guid.NewGuid() }, "eb.generate-view-pdf.big");
 
-        _channel.Publish(new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }, "POIB-enhanceSign");
-        _channel.Publish(new[] { Guid.NewGuid() }, "UCFK-enhanceSign");
+        publisher.Publish(new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }, "ucfk.prepare.small");
 
-        Assert.Pass();
+        publisher.Publish(new[] { Guid.NewGuid() }, "gmu.prepare.normal");
+        publisher.Publish(new[] { Guid.NewGuid() }, "gmu.generate-view-html.big");
+        publisher.Publish(new[] { Guid.NewGuid() }, "gmu.generate-view-pdf.big");
     }
 
-    private void verifyTransformSignature(Guid[] guid, string? channel) { }
+    private void processDefault(Guid guid, string? queue) 
+    {
+        var arr = guid.ToByteArray();
 
-    private void enhanceSign(Guid[] guid, string? channel) { }
+        var type = arr[^1];
 
-    private void cleanRequest(Guid[] guid, string? channel) { }
+        if (type == 1) verifyTransformSignature(guid, queue);
+        if (type == 2) enhanceSign(guid, queue);
+        if (type == 3) cleanRequest(guid, queue);
+    }
+
+    private void enhanceSign(Guid guid, string? queue) { }
+
+    private void prepareSign(Guid guid, string? queue) { }
+
+    private void generateView(Guid guid, string? queue) { }
 }
